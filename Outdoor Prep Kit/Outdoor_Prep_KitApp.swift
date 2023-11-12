728d5ecb94dd47049f012cfc49d555aa46e52a69
21johnh21 <121459080+21johnh21@users.noreply.github.com>
2023-11-11 19:46:16 -0500

    Initial Commit

---
+++ Outdoor Prep Kit/Outdoor_Prep_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outdoor_Prep_KitApp.swift
+//  Outdoor Prep Kit
+//
+//  Created by John Hawley on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Outdoor_Prep_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
