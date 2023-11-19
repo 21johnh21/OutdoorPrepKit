a1becb6adea514157639f20c0a37a5200bd4b286
21johnh21 <121459080+21johnh21@users.noreply.github.com>
2023-11-19 11:01:19 -0500

    Extract ItemListView from TripDetailView.

--- Outdoor Prep Kit/Views/TripViews/TripDetail.swift
+++ Outdoor Prep Kit/Views/TripViews/TripDetail.swift
@@ -38,28 +38,7 @@ struct TripDetail: View {
                 }
             }
             .padding()
-            List{
-                ForEach($itemManager.items) { $item in
-                    NavigationLink(destination: ItemDetail(item: item)){
-                        ItemCard(item: item)
-                    }
-                }
-                .onDelete { indexSet in
-                    itemManager.items.remove(atOffsets: indexSet)
-                }
-                .swipeActions(edge: .leading){
-                    Button(action: {
-                        print("Pack")}) {
-                        Image(systemName: "backpack.circle.fill")
-                        //TODO: Make this so swipping all the way to the right packs the item
-                    }
-                    Button(action: {
-                        print("Edit")
-                        isAddingNewItem = true }) {
-                        Image(systemName: "pencil.circle.fill")
-                    }
-                }
-            }
+            ItemsListView(items: $itemManager.items, isAddingNewItem: $isAddingNewItem)
             .sheet(isPresented: $isAddingNewItem) {
                 ItemEdit(items: $itemManager.items, addingNewItem: $isAddingNewItem, tripID: trip.id)
             }
